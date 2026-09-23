df02e7f4f03d8ee48d6ee1171bd36909c482d836
Shiv <shpate32@ncsu.edu>
2026-09-22 21:03:50 -0400

    Implement Conversation Rule of Five

--- src/conversation.cpp
+++ src/conversation.cpp
@@ -8,6 +8,67 @@ Conversation::~Conversation() {
     delete[] data_;
 }
 
+Conversation::Conversation(const Conversation& other) {
+    size_ = other.size_;
+    capacity_ = other.capacity_;
+
+    if (capacity_ > 0) {
+        data_ = new Message[capacity_];
+
+        for (std::size_t i = 0; i < size_; i++) {
+            data_[i] = other.data_[i];
+        }
+    }
+}
+
+Conversation& Conversation::operator=(const Conversation& other) {
+    if (this != &other) {
+        Message* new_data = nullptr;
+
+        if (other.capacity_ > 0) {
+            new_data = new Message[other.capacity_];
+
+            for (std::size_t i = 0; i < other.size_; i++) {
+                new_data[i] = other.data_[i];
+            }
+        }
+
+        delete[] data_;
+
+        data_ = new_data;
+        size_ = other.size_;
+        capacity_ = other.capacity_;
+    }
+
+    return *this;
+}
+
+Conversation::Conversation(Conversation&& other) noexcept {
+    data_ = other.data_;
+    size_ = other.size_;
+    capacity_ = other.capacity_;
+
+    other.data_ = nullptr;
+    other.size_ = 0;
+    other.capacity_ = 0;
+}
+
+Conversation& Conversation::operator=(Conversation&& other) noexcept {
+    if (this != &other) {
+        delete[] data_;
+
+        data_ = other.data_;
+        size_ = other.size_;
+        capacity_ = other.capacity_;
+
+        other.data_ = nullptr;
+        other.size_ = 0;
+        other.capacity_ = 0;
+    }
+
+    return *this;
+}
+
 std::size_t Conversation::size() const noexcept {
     return size_;
 }
